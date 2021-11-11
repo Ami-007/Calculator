1cf4e91cda1066b03d46735825dcba00ec7be752
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-11 14:11:26 +0000

    updated styles, added query selectors

---
+++ Calculator_/index.js
@@ -0,0 +1,15 @@
+
+const modeShift = document.querySelector("#toggle")
+const allClear = document.querySelector("#allClear")
+const deleteKey = document.querySelector("#delete")
+const answerKey = document.querySelector("#answer")
+const equalKey = document.querySelector("#equals")
+const decimalKey = document.querySelector("#decimal")
+const numberKeys = document.querySelectorAll(".calculator-grid__buttons__number")
+const operatorKeys = document.querySelectorAll(".calculator-grid__buttons__operation")
+
+console.log(numberKeys);
+
+
+// let currentValue = "";
+// let previousValue = 0;
