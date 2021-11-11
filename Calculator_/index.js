bc0c56d144d0e7c7cd7dfdc6b2d0970af62b971f
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-11 23:46:08 +0000

    Updated button overhaul, for loop wasn't useful for carrying on with operations

--- Calculator_/index.js
+++ Calculator_/index.js
@@ -7,31 +7,64 @@ const equalKey = document.querySelector("#equals")
 const decimalKey = document.querySelector("#decimal")
 const numberKeys = document.querySelectorAll(".calculator-grid__buttons__number")
 const operatorKeys = document.querySelectorAll(".calculator-grid__buttons__operation")
-const screenResult = document.querySelector("#output")
+let screenResult = document.querySelector("#output")
 
 // console.log(numberKeys);
+const maxLength = (numLength) => currentNumber.length < numLength;
+const updateDisplay = (values) => screenResult.innerHTML = values;
+// const numberList = Array.from(numberKeys);
+
+let currentNumber = "";
+let operatorValue = "";
+let previousNumber = "";
+let storedValue = 0;
+let calculatedNumber = 0
+
+numberKeys.forEach((button) => {
+    button.addEventListener("click", (numbers) => {
+      if(maxLength(50)) {
+        !currentNumber ? currentNumber = numbers.target.innerHTML : currentNumber += numbers.target.innerHTML;
+        updateDisplay(currentNumber);
+        console.log(currentNumber);
+      }
+    })
+  })
 
 
-let numberValue = [];
-let previousValue = [];
-
-const numberList = Array.from(numberKeys);
-console.log(numberList);
-
-// numberList.forEach(number => {
-//     console.log(number)
+// numberKeys.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         currentNumber = button
+//         updateDisplay(currentNumber);
+//         console.log(currentNumber);
+//     })
 // })
 
-for(let i = 0; i < numberList.length; i++) {
-    numberList[i].addEventListener("click", () => {
-      numberValue.push(numberList[i].innerHTML)
-      console.log(numberValue)
-      screenResult.innerHTML = `${numberValue.join("")}`
-    })
-  };
 
-//   allClear() {
-//       this.screenResult = ''
-//   }
 
-// const numberKeys.addEventListener("click, => )
+// numberKeys.forEach((number) => {
+//     number.addEventListener("click", (event) => {
+//    currentNumber += event.target.innerHTML
+//     numberKeys.value = screenResult
+// })
+
+// });
+
+// console.log(numberList);
+
+// // numberList.forEach(number => {
+// //     console.log(number)
+// // })
+
+// // for(let i = 0; i < numberList.length; i++) {
+// //     numberList[i].addEventListener("click", () => {
+// //       numberValue.push(numberList[i].innerHTML)
+// //       console.log(numberValue)
+// //       screenResult.innerHTML = `${numberValue.join("")}`
+// //     })
+// //   };
+
+// //   allClear() {
+// //       this.screenResult = ''
+// //   }
+
+// // const numberKeys.addEventListener("click, () => )
