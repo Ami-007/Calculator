97b34cacff492686ec063f8111d6a797e331d1a6
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-11 15:28:00 +0000

    Updated HTML to reflect output easier, buttons now supply number

--- Calculator_/index.js
+++ Calculator_/index.js
@@ -7,9 +7,31 @@ const equalKey = document.querySelector("#equals")
 const decimalKey = document.querySelector("#decimal")
 const numberKeys = document.querySelectorAll(".calculator-grid__buttons__number")
 const operatorKeys = document.querySelectorAll(".calculator-grid__buttons__operation")
+const screenResult = document.querySelector("#output")
 
-console.log(numberKeys);
+// console.log(numberKeys);
 
 
-// let currentValue = "";
-// let previousValue = 0;
+let numberValue = [];
+let previousValue = [];
+
+const numberList = Array.from(numberKeys);
+console.log(numberList);
+
+// numberList.forEach(number => {
+//     console.log(number)
+// })
+
+for(let i = 0; i < numberList.length; i++) {
+    numberList[i].addEventListener("click", () => {
+      numberValue.push(numberList[i].innerHTML)
+      console.log(numberValue)
+      screenResult.innerHTML = `${numberValue.join("")}`
+    })
+  };
+
+//   allClear() {
+//       this.screenResult = ''
+//   }
+
+// const numberKeys.addEventListener("click, => )
