bb158ccef13400d946447288c09cf846174dae7a
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-14 20:43:19 +0000

    Added values to my HTML for operators. Added functionality for operator keys! Removed commented out code

--- Calculator_/index.js
+++ Calculator_/index.js
@@ -1,3 +1,4 @@
+// Defining my variables and selecting from HTML 
 
 const modeShift = document.querySelector("#toggle")
 const allClear = document.querySelector("#allClear")
@@ -9,62 +10,34 @@ const numberKeys = document.querySelectorAll(".calculator-grid__buttons__number"
 const operatorKeys = document.querySelectorAll(".calculator-grid__buttons__operation")
 let screenResult = document.querySelector("#output")
 
-// console.log(numberKeys);
-const maxLength = (numLength) => currentNumber.length < numLength;
+const maxLength = (numberLength) => currentNumber.length < numberLength;
 const updateDisplay = (values) => screenResult.innerHTML = values;
-// const numberList = Array.from(numberKeys);
 
 let currentNumber = "";
 let operatorValue = "";
 let previousNumber = "";
 let storedValue = 0;
-let calculatedNumber = 0
+let calculatedNumber = 0;
+
+
 
 numberKeys.forEach((button) => {
     button.addEventListener("click", (numbers) => {
-      if(maxLength(50)) {
+      if(maxLength(20)) {
+        (!currentNumber) ? currentNumber = numbers.target.innerHTML : currentNumber += numbers.target.innerHTML;
+        updateDisplay(currentNumber);
+        console.log(currentNumber);
+      }
+    })
+  });
+
+
+operatorKeys.forEach((button) => {
+    button.addEventListener("click", (numbers) => {
+      if(maxLength(25)) {
         !currentNumber ? currentNumber = numbers.target.innerHTML : currentNumber += numbers.target.innerHTML;
         updateDisplay(currentNumber);
         console.log(currentNumber);
       }
     })
-  })
-
-
-// numberKeys.forEach((button) => {
-//     button.addEventListener("click", () => {
-//         currentNumber = button
-//         updateDisplay(currentNumber);
-//         console.log(currentNumber);
-//     })
-// })
-
-
-
-// numberKeys.forEach((number) => {
-//     number.addEventListener("click", (event) => {
-//    currentNumber += event.target.innerHTML
-//     numberKeys.value = screenResult
-// })
-
-// });
-
-// console.log(numberList);
-
-// // numberList.forEach(number => {
-// //     console.log(number)
-// // })
-
-// // for(let i = 0; i < numberList.length; i++) {
-// //     numberList[i].addEventListener("click", () => {
-// //       numberValue.push(numberList[i].innerHTML)
-// //       console.log(numberValue)
-// //       screenResult.innerHTML = `${numberValue.join("")}`
-// //     })
-// //   };
-
-// //   allClear() {
-// //       this.screenResult = ''
-// //   }
-
-// // const numberKeys.addEventListener("click, () => )
+  });
